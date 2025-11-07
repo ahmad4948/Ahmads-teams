48a561d5ef75fc120303fb007324d6aac28216e3
ahmad4948 <ahmadshamekh99@gmail.com>
2025-11-07 16:31:19 +0200

    Added Header Layout

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,7 +1,18 @@
 function Header(){
 return(
-<div>
- header
+<div className="flex justify-around py-6 font-mont bg-white">
+    
+ <div className="flex items-center gap-3"><img src="ball.png" className="h-9"/>
+ <span className="text-[#28282C] text-2xl font-semibold">Ahmad's Teams</span> </div>
+ <span className="text-[#28282C] text-2xl font-semibold">Home</span>
+ <span className="text-[#28282C] text-2xl font-semibold">Team</span>
+ <span className="text-[#28282C] text-2xl font-semibold">Tickets</span>
+ <span className="text-[#28282C] text-2xl font-semibold">Shop</span>
+ <div className="flex">
+    <img src="facebook.png" className="h-10"/>
+    <img src="twitter.png" className="h-10"/>
+    <img src="linkedin.png" className="h-10"/>
+ </div>
 </div>
 )
 }
