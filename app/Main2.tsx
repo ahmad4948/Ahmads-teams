9e761e09a4ed0169324d2efd015a3f6a27433432
ahmad4948 <ahmadshamekh99@gmail.com>
2025-11-09 10:11:22 +0200

    Filtered the Matches to only manutd and liverpool and added them

--- app/Main2.tsx
+++ app/Main2.tsx
@@ -3,10 +3,61 @@ async function Main(){
        "https://raw.githubusercontent.com/openfootball/football.json/master/2024-25/en.1.json"
     );
     const data = await res.json();
-    console.log(data);
+const teamIDs: Record<string, number> = {
+  "Arsenal FC": 42,
+  "Aston Villa FC": 66,
+  "Bournemouth": 35,
+  "Brentford FC": 55,
+  "Brighton & Hove Albion FC": 51,
+  "Burnley FC": 44,
+  "Chelsea FC": 49,
+  "Crystal Palace FC": 52,
+  "Everton FC": 45,
+  "Fulham FC": 36,
+  "Liverpool FC": 40,
+  "Luton Town FC": 1359,
+  "Manchester City FC": 50,
+  "Manchester United FC": 33,
+  "Newcastle United FC": 34,
+  "Nottingham Forest FC": 65,
+  "Sheffield United FC": 62,
+  "Tottenham Hotspur FC": 47,
+  "West Ham United FC": 48,
+  "Wolverhampton Wanderers FC": 39,
+  "Southampton FC": 41,
+  "Leicester City FC": 46,
+  "AFC Bournemouth": 35,
+};
+  const filtered:any[] = [];
+  for(let i=0;i<data.matches.length;i++){
+    const m = data.matches[i];
+    if(m.team1 == "Liverpool FC" || m.team2 =="Liverpool FC" || 
+    m.team1 == "Manchester United FC" || m.team2 == "Manchester United FC"){
+        filtered.push(m);
+    }
+  }
+  console.log(filtered);
 return(
     <div>
-        this is main
+     {filtered.map((match,i) => {
+      return(
+        <div key={i} className="flex justify-around mt-32 bg-white">
+       <img src={match.team1 === "Ipswich Town FC"? 
+       "https://upload.wikimedia.org/wikipedia/en/4/43/Ipswich_Town.svg"
+      : `https://media.api-sports.io/football/teams/${teamIDs[match.team1]}.png` } 
+        className="h-38"/>
+
+           <span className=" text-4xl text-[#28282C] font-semibold">{match.team1} </span> 
+           <span className=" text-3xl">{match.date} </span> 
+           <span className=" text-4xl text-[#28282C] font-semibold"> {match.team2} </span>
+               <img src={match.team2 === "Ipswich Town FC"? 
+       "https://upload.wikimedia.org/wikipedia/en/4/43/Ipswich_Town.svg"
+      : `https://media.api-sports.io/football/teams/${teamIDs[match.team2]}.png` }
+          className="h-38" />
+           </div>
+      )
+     }
+     )}
     </div>
 )
 }
